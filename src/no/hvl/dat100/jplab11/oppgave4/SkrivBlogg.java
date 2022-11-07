c003f0b7290ad889081121510b8ddacdf3d58d56
OleMedK <97681089+h180786@users.noreply.github.com>
2022-11-07 22:26:41 +0100

    Fikset oppgave 4

--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -5,15 +5,28 @@ import no.hvl.dat100.jplab11.oppgave3.*;
 
 public class SkrivBlogg{
 
-	public static boolean skriv(Blogg samling, String mappe, String filnavn) throws Exception {
+	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
 	
-		BufferedWriter bwriter = new BufferedWriter(new FileWriter(mappe + filnavn));
-
-		bwriter.write(samling.toString());
-		bwriter.close();
+		File file = new File(mappe + filnavn);
+		FileWriter writer = null;
+		try {
+			writer = new FileWriter(file);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		PrintWriter print = new PrintWriter(writer);
+		
+		print.write(samling.toString());
+		print.close();
+		
 		System.out.println(samling.toString());
+		
 		return true;
 		
+		//Alternativ måte, men må ha throws exception på test
+//		BufferedWriter bwriter = new BufferedWriter(new FileWriter(mappe + filnavn));
+//		bwriter.write(samling.toString());
+//		bwriter.close();
 	}
 
 }
